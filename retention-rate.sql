8bbf78dc84c858f180230d250b724fb06ce6cc35
daria-sidorova <poznavatelnik0@gmail.com>
2025-03-08 21:43:59 +0300

    Update retention-rate.sql

--- retention-rate.sql
+++ retention-rate.sql
@@ -7,14 +7,14 @@ WITH daily_active_users AS (
 ),
 retention AS (
     SELECT 
-        login_date AS day_0,
-        uniqExact(player_id) AS new_users,
-        uniqExactIf(player_id, player_id IN (
-            SELECT player_id FROM daily_active_users da2 WHERE da2.login_date = da1.login_date + 1
-        )) AS retained_users,
+        day_0.login_date AS day_0,
+        uniqExact(day_0.player_id) AS new_users,
+        uniqExactIf(day_0.player_id, day_0.player_id IN (
+            SELECT player_id FROM daily_active_users da2 WHERE da2.login_date = day_0.login_date + 7
+        )) AS retained_users, -- Checking for return on the 7th day
         ROUND(100.0 * retained_users / new_users, 2) AS retention_rate
-    FROM daily_active_users da1
-    GROUP BY login_date
+    FROM daily_active_users day_0
+    GROUP BY day_0.login_date
 )
 SELECT * FROM retention
 ORDER BY day_0;
